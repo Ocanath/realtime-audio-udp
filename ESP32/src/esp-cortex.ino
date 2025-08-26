6cdb97c12dfa1eef48f708c124d6167a3a2bbe5f
ocanath <ocanath@gmail.com>
2025-08-26 12:29:51 -0700

    I2S added

--- ESP32/src/esp-cortex.ino
+++ ESP32/src/esp-cortex.ino
@@ -28,20 +28,20 @@ IPAddress server_address; //note: may want to change to our local IP, to support
 
 // you shouldn't need to change these settings
 #define SAMPLE_BUFFER_SIZE 512
-#define SAMPLE_RATE 8000
+#define SAMPLE_RATE 16000
 // most microphones will probably default to left channel but you may need to tie the L/R pin low
 #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
 // either wire your microphone to the same pins or change these to match your wiring
-#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_15
-#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_0
-#define I2S_MIC_SERIAL_DATA GPIO_NUM_14
+#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_14		//SCK
+#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_13	//WS
+#define I2S_MIC_SERIAL_DATA GPIO_NUM_12			//SD
 
 // don't mess around with this
 i2s_config_t i2s_config = {
     .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
     .sample_rate = SAMPLE_RATE,
     .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+    .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
     .communication_format = I2S_COMM_FORMAT_STAND_I2S,
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
     .dma_buf_count = 4,
@@ -133,14 +133,15 @@ int32_t raw_samples[SAMPLE_BUFFER_SIZE] = {};
 void loop() 
 {
 
-  size_t bytes_read = 0;
-  i2s_read(I2S_NUM_0, raw_samples, sizeof(int32_t) * SAMPLE_BUFFER_SIZE, &bytes_read, portMAX_DELAY);
-  int samples_read = bytes_read / sizeof(int32_t);
+	/**/
+	size_t bytes_read = 0;
+	i2s_read(I2S_NUM_0, raw_samples, sizeof(int32_t) * SAMPLE_BUFFER_SIZE, &bytes_read, portMAX_DELAY);
+	int samples_read = bytes_read / sizeof(int32_t);
   // dump the samples out to the serial channel.
-  for (int i = 0; i < samples_read; i++)
-  {
-    Serial.printf("%ld\n", raw_samples[i]);
-  }
+//   for (int i = 0; i < samples_read; i++)
+//   {
+//     Serial.printf("%ld\n", raw_samples[i]);
+//   }
 
 
   
